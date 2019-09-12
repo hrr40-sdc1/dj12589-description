fd92a10240ec74bf4a4d562a83d7baa70d9c918b
dj12589 <daria.jammal@gmail.com>
2019-09-11 20:31:28 -0700

    api routes - initial crud

---
+++ database/test-data.js
@@ -0,0 +1,151 @@
+const testHouse = [{
+  "house_id":300,
+  "title":"Dui Nec Urna Suscipit Nonummy. Fusce Fermentum Fermentum",
+  "location":"Virton",
+  "is_entire_place":false,
+  "private_room":{
+    "guest":1,
+    "bedrooms":[
+      {
+        "beds":[
+          {
+            "size":"king"
+          }
+        ]
+      },
+      {
+        "beds":[
+          {
+            "size":"king"
+          }
+        ]
+      },
+      {
+        "beds":[
+          {
+            "size":"normal"
+          },
+          {
+            "size":"king"
+          }
+        ]
+      }
+    ],
+    "bath":1
+  },
+  "super_host_name":"Dalton Lee",
+  "super_host_photo":"super-host-photo-1.jpg",
+  "rating":25,
+  "desc":"rutrum magna. Cras convallis convallis dolor. Quisque tincidunt pede ac urna. Ut tincidunt vehicula risus. Nulla eget metus eu erat semper rutrum. Fusce dolor quam, elementum at, egestas a, scelerisque sed,",
+  "space_desc":"tristique pellentesque, tellus sem mollis dui, in sodales elit erat vitae risus. Duis a mi fringilla mi lacinia mattis. Integer eu lacus. Quisque imperdiet, erat nonummy ultricies ornare, elit elit fermentum risus, at fringilla purus mauris a nunc. In at pede. Cras vulputate velit eu sem. Pellentesque ut ipsum ac mi eleifend egestas. Sed pharetra, felis eget varius ultrices, mauris ipsum porta elit, a feugiat tellus lorem eu metus. In lorem. Donec elementum, lorem ut aliquam iaculis, lacus pede sagittis augue, eu tempor erat neque non quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam fringilla cursus purus. Nullam scelerisque neque sed sem egestas blandit. Nam nulla magna, malesuada vel, convallis in, cursus et, eros. Proin ultrices. Duis volutpat nunc sit amet metus. Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt nibh. Phasellus nulla. Integer vulputate, risus a ultricies adipiscing, enim mi tempor lorem, eget mollis lectus pede et risus. Quisque libero lacus, varius et, euismod et, commodo at, libero. Morbi accumsan laoreet ipsum. Curabitur consequat, lectus sit amet luctus vulputate, nisi sem semper erat, in consectetuer ipsum nunc id enim. Curabitur massa. Vestibulum accumsan neque et nunc. Quisque ornare tortor at risus. Nunc ac sem ut dolor dapibus gravida. Aliquam tincidunt, nunc ac mattis ornare, lectus ante dictum mi, ac mattis velit justo nec ante. Maecenas mi felis, adipiscing fringilla, porttitor vulputate, posuere vulputate, lacus. Cras interdum. Nunc sollicitudin commodo ipsum. Suspendisse non leo. Vivamus nibh dolor, nonummy ac, feugiat non, lobortis quis, pede. Suspendisse dui. Fusce diam nunc, ullamcorper eu, euismod ac, fermentum vel, mauris. Integer sem elit, pharetra ut, pharetra sed, hendrerit a, arcu. Sed et libero. Proin mi. Aliquam gravida mauris ut mi. Duis risus odio, auctor vitae, aliquet nec, imperdiet nec, leo. Morbi neque tellus, imperdiet non, vestibulum nec, euismod in, dolor. Fusce feugiat. Lorem ipsum",
+  "guest_desc":"In tincidunt congue turpis. In condimentum. Donec at arcu. Vestibulum ante ipsum primis in faucibus orci",
+  "other_desc":"dapibus id, blandit at, nisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin vel nisl. Quisque fringilla euismod enim. Etiam gravida molestie arcu. Sed",
+  "amenities":[
+    {
+      "category":"Basic",
+      "item":"Wifi",
+      "desc":"Continuous access in the listing"
+    },
+    {
+      "category":"Basic",
+      "item":"Iron",
+      "desc":""
+    },
+    {
+      "category":"Basic",
+      "item":"Dryer",
+      "desc":"In the building, free or for a fee"
+    },
+    {
+      "category":"Basic",
+      "item":"Heating",
+      "desc":"Central heating or a heater in the listing"
+    },
+    {
+      "category":"Basic",
+      "item":"TV",
+      "desc":""
+    },
+    {
+      "category":"Basic",
+      "item":"Air conditioning",
+      "desc":""
+    },
+    {
+      "category":"Dining",
+      "item":"Kitchen",
+      "desc":"Space where guests can cook their own meals"
+    },
+    {
+      "category":"Logistics",
+      "item":"Luggage dropoff allowed",
+      "desc":"For guests' convenience when they have early arrival or late departure"
+    },
+    {
+      "category":"Bed and bath",
+      "item":"Hangers",
+      "desc":""
+    },
+    {
+      "category":"Bed and bath",
+      "item":"Shampoo",
+      "desc":""
+    },
+    {
+      "category":"Not included",
+      "item":"Private entrance",
+      "desc":""
+    }
+  ],
+  "availability":[
+    "2019-8-27",
+    "2019-9-1",
+    "2019-9-5",
+    "2019-9-10",
+    "2019-9-13",
+    "2019-9-14",
+    "2019-9-15",
+    "2019-9-17",
+    "2019-9-21",
+    "2019-9-22",
+    "2019-9-24",
+    "2019-10-1",
+    "2019-10-8",
+    "2019-10-10",
+    "2019-10-13",
+    "2019-10-16",
+    "2019-10-17",
+    "2019-10-18",
+    "2019-10-19",
+    "2019-10-20",
+    "2019-10-21",
+    "2019-10-22",
+    "2019-10-23",
+    "2019-10-24",
+    "2019-10-25",
+    "2019-10-26",
+    "2019-10-27",
+    "2019-10-29",
+    "2019-10-30",
+    "2019-10-31",
+    "2019-11-1",
+    "2019-11-3",
+    "2019-11-4",
+    "2019-11-5",
+    "2019-11-6",
+    "2019-11-7",
+    "2019-11-11",
+    "2019-11-12",
+    "2019-11-13",
+    "2019-11-14",
+    "2019-11-15",
+    "2019-11-16",
+    "2019-11-17",
+    "2019-11-18",
+    "2019-11-19",
+    "2019-11-20"
+  ]
+}
+]
+
+module.exports.testHouse = testHouse;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 require('dotenv').config();
 var cors = require('./cors');
+const bodyParser = require('body-parser')
+const testHouse = require('./../database/test-data.js')
 
 const House = require('./../database/House.js');
 const Photo = require('./../database/Photo.js');
@@ -14,17 +16,62 @@ app.use(express.static(__dirname + '/../client/dist'));
 // serve static image files in public if necessary
 app.use(express.static(__dirname + '/../public'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 // API Endpoints
 app.get('/houses/:id', (req, res, next) => {
   var houseId = req.params.id;
 
-  House.findOne({ house_id: houseId }).populate('photos').exec((err, house) => {
-    if (err) {
+  House.findOne({ house_id: houseId }, (err, house) => {
+    if (err || !house) {
       console.log('error fetching this house', err);
       res.status(400).json({ success: false, message: 'Could not fetch this House from our Database' });
     } else {
       res.status(200).json(house);
+    }
+  });
+});
+
+app.post('/houses/:id', (req, res, next) => {
+  const houseId = req.params.id
+  House.findOne({ house_id: houseId }, (err, results) => {
+    if (err || !results) {
+      console.log('what i want to put in', testHouse.testHouse[0])
+      House.create(testHouse.testHouse[0], (err, house) => {
+        console.log('house in house create', house)
+        if (err) {
+          res.end('Error creating house.');
+        } else {
+          res.status(200).json(house);
+        }
+      })
+    } else {
+      res.end('House already exists.');
+    }
+  });
+});
+
+app.put('/houses/:id', (req, res, next) => {
+  const houseId = req.params.id
+  var location = {location: 'Brisbane'}
+      House.findOneAndUpdate({house_id: houseId}, location, (err, house) => {
+        if (err) {
+          res.send('Error updating house.');
+        } else {
+          res.status(200).json(house);
+        }
+      })
+});
+
+app.delete('/houses/:id', (req, res, next) => {
+  const houseId = req.params.id
+  House.deleteOne({ house_id: houseId }, (err, result) => {
+    if (err || !result) {
+      res.status(404).send('No house found to delete.')
+    } else {
+      res.send('House successfully deleted.')
     }
   });
 });
